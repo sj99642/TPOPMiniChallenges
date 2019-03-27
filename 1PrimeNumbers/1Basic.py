4bfc18747abba72bb57dd17b06be2fbfc0e4d9ee
Stephen <stephenj99642@gmail.com>
2019-03-27 21:02:42 +0000

    Initial commit; set up files for basic task on challenge 1.

---
+++ 1PrimeNumbers/1Basic.py
@@ -0,0 +1,23 @@
+"""
+A prime number (or a prime) is a natural number greater than 1 that has
+no positive divisors other than 1 and itself.
+
+The property of being prime is called primality. A simple but slow method of
+verifying the primality of a given number is known as trial division. It
+consists of testing whether n is a multiple of any integer between 2 and sqrt(n).
+
+# The Basic
+
+Write a program that, given a number comprised between 2 and 49, returns
+if it is a prime number or not. We can assume that the computer knows
+(stores) that [2, 3, 5, 7] are prime numbers.
+"""
+
+def is_prime(n: int) -> bool:
+    pass
+
+def main():
+    pass
+
+if __name__ == "__main__":
+    main()
--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # TPOPMiniChallenges
 The Mini Challenges from University of York Theory and Practice of Programming.
+
+The challenges are [here](https://vle.york.ac.uk/webapps/blackboard/content/listContent.jsp?course_id=_88692_1&content_id=_2846014_1&mode=reset).
+
