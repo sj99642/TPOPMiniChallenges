55a397ead26b0d43339d7b77fd3af945c71cce7c
Stephen <stephenj99642@gmail.com>
2019-03-27 21:34:02 +0000

    Implemented Advanced difficulty for Challenge 1

---
+++ 1PrimeNumbers/Advanced.py
@@ -0,0 +1,22 @@
+"""
+Write a program that, given a number greater than 2, returns if it is a
+prime number or not. We can assume that the computer at the start knows
+only that 2 is prime number. We should use a loop to test several numbers.
+"""
+
+import math
+
+def is_prime(n: int) -> bool:
+    for i in range(2, int(math.sqrt(n))+1):
+        if n % i == 0:
+            return False
+    return True
+
+
+def main():
+    n = int(input("Enter a number: "))
+
+    if is_prime(n):
+        print(n, "is prime")
+    else:
+        print(n, "is not prime")
