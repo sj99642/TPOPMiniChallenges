bd2441303e2433ce9ee593d5d62eb508b7384149
Stephen Jones <stephenj99642@gmail.com>
2019-06-01 22:18:11 +0100

    Sending all changes to git from uni, including starting on the maze

---
+++ TurtleAndMaze/maze.py
@@ -0,0 +1,11 @@
+"""
+Defines the Maze data type, which can store and draw a maze of arbitrary size.
+"""
+
+class Maze:
+    def __init__(self, size=(8, 8)):
+        """
+        Creates a blank maze
+        :param size:
+        """
+        pass
